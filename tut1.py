dba533c2b4523cac94646a14ad82214e1e8c3754
AmnaHussien <amnahussin88@gmail.com>
2025-10-16 21:59:20 +0200

    d

---
+++ tut1.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect, url_for
+
+#create instance of flask web app
+app = Flask(__name__)
+
+#define home page, path of the func in web:wq!
+@app.route("/")
+def home():
+	return"Hello world"
+#page accept param
+@app.route("/<name>")
+def user(name):
+	return f"hello {name}"
+@app.route("/admin")
+def admin():
+	return redirect(url_for("home"))
+
+if __name__ == "__main__":
+	app.run()
